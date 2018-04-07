d47bb7acd774ef908cb2617efc65aca9ef040818
re0marb1e <zhxt1016@gmail.com>
2018-04-07 23:03:23 +0800

    add parallel test

--- test/test.js
+++ test/test.js
@@ -2,12 +2,29 @@ const flowControl = require('../index');
 
 flowControl.series([
   function (callback, result){
-    console.log('test 1');
-    callback('Hello, test 2!');
+    setTimeout(function (){
+      console.log('series test 1');
+      callback('series test 1 => series test 2');
+    }, 200);
   },
   function (callback, result){
-    console.log(result);
-    console.log('test 2');
-    callback();
+    setTimeout(function (){
+      console.log(result);
+      console.log('series test 2');
+      callback();
+    }, 100);
+  }
+]);
+
+flowControl.parallel([
+  function (callback){
+    setTimeout(function (){
+      console.log('parallel test 1');
+    }, 200);
+  },
+  function (callback){
+    setTimeout(function (){
+      console.log('parallel test 2');
+    }, 100);
   }
 ]);
